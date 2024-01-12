ccf7a3bcb10b5927712971c40c065b9e7eb100ff
tzuen <codelearner2424@gmail.com>
2024-01-12 22:58:39 +0800

    Created scalable grids and added basic functions of etch-a-sketch

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+const gridLength = document.querySelector("#grid-length");
+
+
+function createGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < n; j++) {
+            const cell = document.createElement("div");
+            cell.style.width = `${36/n}rem`;
+            cell.style.height = `${36/n}rem`;
+            cell.style.border = "none";
+            cell.classList.add("grid-cell");
+            row.appendChild(cell);
+
+        }
+        container.appendChild(row);
+    }
+}
+
+function reset() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    createGrid(`${gridLength.value}`)
+
+
+}
+createGrid(8);
+container.addEventListener("mouseover", function(e) {
+    if (e.target.matches(".grid-cell")) {
+        e.target.classList.add('active');
+    }
+})
+
+gridLength.addEventListener('input', () => {
+    reset();
+    console.log(`${gridLength.value}`)
+})
